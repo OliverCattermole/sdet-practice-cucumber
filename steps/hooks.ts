4cb3b8bcd81d1e7dfb48bc1b09c4921e4be2f16e
OliverCattermole <oliver.cattermole@instavolt.co.uk>
2026-04-19 11:14:21 +0100

    Updated headless mode (true for cloud, false for local)

--- steps/hooks.ts
+++ steps/hooks.ts
@@ -5,7 +5,7 @@ import { LoginPage } from "../pages/LoginPage";
 import { ProductsPage } from "../pages/ProductsPage";
 
 Before(async function (this: CustomWorld) {
-    this.browser = await chromium.launch({ headless: false });
+    this.browser = await chromium.launch({ headless: process.env.CI ? true : false });
     this.context = await this.browser.newContext();
     this.page = await this.context.newPage();
     this.loginPage = new LoginPage(this.page);
